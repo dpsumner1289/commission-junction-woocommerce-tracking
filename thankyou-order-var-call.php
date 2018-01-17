c108eb163dbe19e159b2a6ce11ab1e99d87d282d
dpsumner1289 <dpsumner1289@gmail.com>
2018-01-17 15:48:25 -0500

    Create thankyou-order-var-call.php

---
+++ thankyou-order-var-call.php
@@ -0,0 +1,6 @@
+// Place this code in the thankyou.php file, which should have been copied into your child theme.
+// Be sure to place it at the top of the file, underneath the 
+
+<?php
+$order_id = $order->id
+?>
